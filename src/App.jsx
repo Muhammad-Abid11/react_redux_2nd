f9eba7080d023e2c5b728e8db0e7bcba003ece56
Muhammad Abid <abidkhan11845@gmail.com>
2022-05-31 03:37:24 +0500

    React_redux start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Hello React Redux`</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
