611c8d8666b84a537ab5f53ab7c4140d065f980d
Muhammad Abid <abidkhan11845@gmail.com>
2022-05-31 04:06:40 +0500

    now we have multiple store so first connect them by "index file" in reducer folder

---
+++ src/store/reducers/app_reducer.jsx
@@ -0,0 +1,9 @@
+// store 
+
+const Initial_data = {
+    app_name: "Angry Bird",
+}
+
+export default (state = Initial_data) => {
+    return state;
+}
---
+++ src/store/reducers/auth_reducer.jsx
@@ -0,0 +1,10 @@
+// store 
+
+const Initial_data = {
+    username: "Sajid",
+    email: "sajid@gmail.com",
+}
+
+export default (state = Initial_data) => {
+    return state;
+}
