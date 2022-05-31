e015e5c7f214002d8515f0c5b5942982253f3454
Muhammad Abid <abidkhan11845@gmail.com>
2022-05-31 06:11:20 +0500

    dispatch from "action files" receive in "action parameter" in "EveryReducers"

--- src/store/reducers/reducer.jsx
+++ src/store/reducers/reducer.jsx
@@ -4,6 +4,7 @@ const Initial_data = {
     name: "red_Sajid",
 }
 
-export default (state = Initial_data) => {
+export default (state = Initial_data, action) => {
+    console.log("action from reducer==>", action)
     return state;
 }
